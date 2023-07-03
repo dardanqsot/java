ff8f5e2eb634670702a1614c8debcfb9f6ffb6d0
dardanqsot <quispesotodaniel@gmail.com>
2023-07-02 19:54:39 -0500

    EJB - Using EJB, @Stateless, @Stateful, @Local

--- 1_webapp-jsf3/webapp-jsf3/src/main/java/org/dardan/webapp/jsf3/services/ProductoServiceImpl.java
+++ 1_webapp-jsf3/webapp-jsf3/src/main/java/org/dardan/webapp/jsf3/services/ProductoServiceImpl.java
@@ -3,6 +3,7 @@ package org.dardan.webapp.jsf3.services;
 import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
 import org.dardan.webapp.jsf3.entities.Producto;
+import org.dardan.webapp.jsf3.repositories.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,14 +11,16 @@ import java.util.Optional;
 @Stateless
 public class ProductoServiceImpl implements ProductoService {
 
+    @Inject
+    private CrudRepository<Producto> repository;
 
     @Override
     public List<Producto> listar() {
-        return null;
+        return repository.listar();
     }
 
     @Override
     public Optional<Producto> porId(Long id) {
-        return Optional.empty();
+        return Optional.ofNullable(repository.porId(id));
     }
 }
---
+++ webapp-ejb/webapp-ejb/src/main/java/org/dardan/webapp/ejb/controllers/EjemploServlet.java
@@ -0,0 +1,41 @@
+package org.dardan.webapp.ejb.controllers;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.dardan.webapp.ejb.models.Producto;
+import org.dardan.webapp.ejb.service.ServiceEjbLocal;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.io.IOException;
+
+@WebServlet("/index")
+public class EjemploServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        ServiceEjbLocal service = null;
+        ServiceEjbLocal service2 = null;
+        try {
+            InitialContext ctx = new InitialContext();
+            service = (ServiceEjbLocal) ctx.lookup("java:global/webapp-ejb/ServiceEjb!org.dardan.webapp.ejb.service.ServiceEjbLocal");
+            service2 = (ServiceEjbLocal) ctx.lookup("java:global/webapp-ejb/ServiceEjb!org.dardan.webapp.ejb.service.ServiceEjbLocal");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+        System.out.println("service si es igual a service2 = " + service.equals(service2));
+
+        Producto  p = service.crear(new Producto("uvas"));
+        System.out.println("nuevo producto " + p);
+
+        req.setAttribute("saludo", service.saludar("Darwin"));
+        req.setAttribute("saludo2", service2.saludar("John"));
+        req.setAttribute("listado", service.listar());
+        getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
+    }
+}
