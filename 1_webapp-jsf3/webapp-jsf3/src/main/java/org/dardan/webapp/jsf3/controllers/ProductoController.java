35a838f0c6e3e9583030a26b76569649b085c8b6
dardanqsot <quispesotodaniel@gmail.com>
2023-07-11 22:30:42 -0500

    JSF - Form CRUD Complete

--- 1_webapp-jsf3/webapp-jsf3/src/main/java/org/dardan/webapp/jsf3/controllers/ProductoController.java
+++ 1_webapp-jsf3/webapp-jsf3/src/main/java/org/dardan/webapp/jsf3/controllers/ProductoController.java
@@ -8,12 +8,15 @@ import jakarta.inject.Named;
 import org.dardan.webapp.jsf3.entities.Producto;
 import org.dardan.webapp.jsf3.services.ProductoService;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Model
 public class ProductoController {
 
     private Producto producto;
+
+    private Long id;
 
     @Inject
     private ProductoService service;
@@ -28,19 +31,43 @@ public class ProductoController {
     @RequestScoped
     @Named("listado")
     public List<Producto> findAll() {
+//        return Arrays.asList(new Producto("peras"), new Producto("manzanas"), new Producto("mandarinas"));
         return service.listar();
     }
 
     @Produces
     @Model
-    public Producto producto(){
+    public Producto producto() {
         this.producto = new Producto();
+        if (id != null && id > 0) {
+            service.porId(id).ifPresent(p -> {
+                this.producto = p;
+            });
+        }
         return producto;
+    }
+
+    public String editar(Long id){
+        this.id = id;
+        return "form.xhtml";
     }
 
     public String guardar() {
         System.out.println(producto);
         service.guardar(producto);
         return "index.xhtml?faces-redirect=true";
+    }
+
+    public String eliminar(Long id){
+        service.eliminar(id);
+        return "index.xhtml?faces-redirect=true";
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
